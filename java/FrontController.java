0e2c422cb43505807df3e9110f71b4aaf6c638dc
valsoa <manoaniainavalisoa@gmail.com>
2024-05-23 09:13:04 +0200

    commit Sprint2_2420

--- java/FrontController.java
+++ java/FrontController.java
@@ -1,8 +1,10 @@
 package util;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.rmi.server.ServerCloneException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import jakarta.servlet.ServletConfig;
@@ -11,35 +13,51 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.Annotation.*;
+import java.lang.annotation.Annotation;
 
 
 public class FrontController extends HttpServlet {
     private List<Class<?>> listeController;
+    private HashMap<String,Mapping> urlMapping = new HashMap<>();
 
     public void init(ServletConfig conf) throws ServletException {
         super.init(conf);
         String packages = this.getInitParameter("package");
-        listeController = allMappingUrls(packages);
+        listeController = allMappingUrls(packages,util.Annotation.AnnotationController.class);
+        urlMapping = getUrlMapping(listeController);
     }
 
-    public List<Class<?>> allMappingUrls(String packageName) {
+    public HashMap<String,Mapping> getUrlMapping(List<Class<?>> listeController){
+        HashMap<String,Mapping> result = new HashMap<>();
+        for (Class<?> class1 : listeController) {
+            Method[] methodes = class1.getDeclaredMethods();
+            for (Method method : methodes) {
+                if(method.isAnnotationPresent(util.Annotation.Get.class)){
+                    result.put(method.getAnnotation(util.Annotation.Get.class).value(),
+                    new Mapping(class1.getSimpleName(),method.getName()));
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Class<?>> allMappingUrls(String packageName, Class<? extends Annotation> annotationClass) {
         listeController = new ArrayList<>();
         ServletContext context = getServletContext();
-        String path = "/WEB-INF/classes/" + packageName;
-
+        String path = "/WEB-INF/classes/" + packageName.replace('.', '/');
+    
         Set<String> classNames = context.getResourcePaths(path);
         if (classNames != null) {
             for (String className : classNames) {
                 if (className.endsWith(".class")) {
-                    String fullClassName = className.substring(0, className.length() - 6);
-                    int taille = fullClassName.split("/").length;
-                    fullClassName = fullClassName.split("/")[taille - 2] + "." + fullClassName.split("/")[taille - 1];
-
+                    String fullClassName = packageName + "." + className.substring(path.length() + 1, className.length() - 6).replace('/', '.');
+                    
                     try {
                         Class<?> clazz = Class.forName(fullClassName);
-                        AnnotationController annotation = clazz.getAnnotation(AnnotationController.class);
-                        if (annotation != null) {
-                            System.out.println("value" + annotation.value());
+                        Annotation annotation = clazz.getAnnotation(annotationClass);
+                        if (annotation instanceof AnnotationController) {
+                            AnnotationController controllerAnnotation = (AnnotationController) annotation;
                             listeController.add(clazz);
                         }
                     } catch (Exception e) {
@@ -51,8 +69,7 @@ public class FrontController extends HttpServlet {
             System.out.println("class null");
         }
         return listeController;
-    }
-      
+    }     
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
@@ -68,7 +85,7 @@ public class FrontController extends HttpServlet {
         resp.getWriter().println("<h1>Lien :" + req.getRequestURI() + "</h1>");
 
         for (Class<?> controllerClass : listeController) {
-            AnnotationController annotation = controllerClass.getAnnotation(AnnotationController.class);
+            AnnotationController annotation = controllerClass.getAnnotation(util.Annotation.AnnotationController.class);
             if (annotation != null) {
                 String nameController = controllerClass.getSimpleName();
                 resp.getWriter().println("controller :" + nameController);
@@ -76,5 +93,6 @@ public class FrontController extends HttpServlet {
                 resp.getWriter().println("Annotation nulle");
             }
         }
+        resp.getWriter().println("urlMapping:"+urlMapping);
     }
 }
